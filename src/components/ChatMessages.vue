2f2e69a4b78ead859e02daf5c5d25eeced63969e
sunoyu <suno_yu@naver.com>
2025-04-01 00:06:49 +0900

    Feat: 메세지 입력시 부드럽게 스크롤링

--- src/components/ChatMessages.vue
+++ src/components/ChatMessages.vue
@@ -1,19 +1,41 @@
 <template>
-  <div class="chat-box">
+  <div class="chat-messages">
     <div
       v-for="(msg, idx) in messages"
       :key="idx"
       class="d-flex mb-2"
       :class="msg.role == 'user' ? 'justify-end' : 'justify-start'"
     >
-      <v-chip color="primary" v-if="msg.role === 'user'">{{ msg.text }}</v-chip>
-      <v-chip color="grey lighten-2" v-else>{{ msg.text }}</v-chip>
+      <v-chip
+        :color="msg.role == 'user' ? 'primary' : 'grey lighten-2'"
+        text-color="white"
+        class="ma-1"
+        >{{ msg.text }}</v-chip
+      >
     </div>
+    <div ref="scrollTarget"></div>
   </div>
 </template>
 
 <script setup>
-defineProps(["messages"]);
+import { ref, watch, nextTick } from "vue";
+const props = defineProps({
+  messages: {
+    type: Array,
+    required: true,
+  },
+});
+
+const scrollTarget = ref(null);
+
+watch(
+  () => props.messages,
+  async () => {
+    await nextTick();
+    scrollTarget.value?.scrollIntoView({ behavior: "smmoth" });
+  },
+  { deep: true }
+);
 </script>
 
 <style scoped>
